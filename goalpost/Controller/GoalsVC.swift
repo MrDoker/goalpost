1dbaa0b63cb895db7225a2f62545060b031b86d8
George Sazonov <g-doker@yandex.ru>
2018-09-05 16:21:40 +0300

    Start creating project UI

--- goalpost/Controller/GoalsVC.swift
+++ goalpost/Controller/GoalsVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalsVC.swift
 //  goalpost
 //
 //  Created by DokeR on 05.09.2018.
@@ -8,13 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func addGoalButtonWasPressed(_ sender: Any) {
+        
+    }
+    
 }
 
